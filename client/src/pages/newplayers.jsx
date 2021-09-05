f313edfc8248d803cd69d5650c1f470868180d90
Zitai Huang <mywpiedu@gmail.com>
2021-09-04 21:17:28 -0500

    answers

--- client/src/pages/newplayers.jsx
+++ client/src/pages/newplayers.jsx
@@ -4,8 +4,8 @@ function NewPlayers(){
 
 
  return <div className="QA">
-     <div className="question">Question: I have never drove on track, how do I start?</div>
-     <div>Answer : You can start by attending the beginner trackday events on you local track, they usually have car and instructor for you to get started.</div>
+     <div className="questions">Question: I have never drove on track, how do I start?</div>
+     <div className="answers">Answer : You can start by attending the beginner trackday events on you local track, they usually have car and instructor for you to get started.</div>
  </div>   
 }
 export default NewPlayers
