194177ed627545dca638f7d6915b163a52efa473
Zitai Huang <mywpiedu@gmail.com>
2021-09-04 21:28:50 -0500

    questions css

--- client/src/pages/newplayers.jsx
+++ client/src/pages/newplayers.jsx
@@ -4,7 +4,7 @@ function NewPlayers(){
 
 
  return <div className="QA">
-     <div className="questions">Question: I have never drove on track, how do I start?</div>
+     <div className="qustions">Question: I have never drove on track, how do I start?</div>
      <div className="answers">Answer : You can start by attending the beginner trackday events on you local track, they usually have car and instructor for you to get started.</div>
  </div>   
 }
